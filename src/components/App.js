9202b7ba37f89b6b9faf824656ddcf4e1349ac1d
danwycliff <danwycliff6769@gmail.com>
2023-08-17 22:57:02 +0300

    codes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Toggle from "./Toggle";
 
+
+
 function App() {
+  function Toggle(){
+  let [isOn,setIsOn] = useState(false);
+
+    function handleClick() {
+      setIsOn((isOn) => !isOn);
+    }
+    const color = isOn ? "red" : "white";
+    return(
+    <button style={{ background:"red" }}onClick={handleClick}>{isOn ?
+       "ON" : "OF"}</button>
+       )
+  }
   return (
     <div>
       <h3>Toggle</h3>
